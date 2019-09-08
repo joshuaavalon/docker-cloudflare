884dec1cecda7f4da563c1ab3ed8e56e2b51fa0f
Andy <andy@savage.hk>
2019-09-08 21:54:50 +0800

    Update cloudflare.sh
    Added API Token support (if email is not set it will be assumed to be an API Token)
    Checked if system contains required programs (sed, jq, curl etc)
    By default checks if API token is valid first. Disable with CHECK_API_TOKEN=false
    Can set IP_FILE to specify where the ip.dat lives (by default it's now in /tmp)
    Can use a custom URL for fetching the IP address by setting CUSTOM_IP_URL=<custom_url>
    Can force a specific IP address to be set using NEW_IP_ADDR=<ip_address> this allows a script to run first and then call this one after
    Can get an ip address from a specific local adapter using USE_ADAPTER=<adapter name> this allows to use for ddns on internal network which gets it's address from dhcp

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Checks for installed programs
+command -v jq >/dev/null 2>&1 || { echo >&2 "I require jq but it's not installed.  Aborting."; exit 1; }
+command -v curl >/dev/null 2>&1 || { echo >&2 "I require curl but it's not installed.  Aborting."; exit 1; }
+
+# MASTER_API_KEY defaults to true
+MASTER_API_KEY=${MASTER_API_KEY:-true}
+
+# CHECK_API_TOKEN defaults to true
+CHECK_API_TOKEN=${CHECK_API_TOKEN:-true}
+
+if [[ "$MASTER_API_KEY" == "true" ]]; then
+  required_vars=(ZONE HOST EMAIL API)
+else
+  required_vars=(ZONE HOST API)
+fi
+
 # Enforces required env variables
-required_vars=(ZONE HOST EMAIL API)
 for required_var in "${required_vars[@]}"; do
     if [[ -z ${!required_var} ]]; then
         error=1
@@ -13,8 +28,26 @@ if [[ -n $error ]]; then
     exit 1
 fi
 
+# Check API Token
+if [[ "$MASTER_API_KEY" != "true" ]] && [[ "$CHECK_API_TOKEN" == "true" ]]; then
+  # Check if API Token is valid
+  is_valid_response=$(curl -s -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" \
+     -H "Authorization: Bearer $API" \
+     -H "Content-Type:application/json")
+
+  # If not successful, errors out
+  if [[ $(jq <<<"$is_valid_response" -r '.success') != "true" ]]; then
+    messages=$(jq <<<"$is_valid_response" -r '[.errors[] | .message] |join(" - ")')
+    echo >&2 "Error: $messages"
+    exit 1
+  fi
+fi
+
 # PROXY defaults to true
 PROXY=${PROXY:-true}
+
+# IP_FILE defaults to /tmp/ip.dat
+IP_FILE=${IP_FILE:-/tmp/ip.dat}
 
 # TTL defaults to 1 (automatic), and is validated
 TTL=${TTL:-1}
@@ -32,30 +65,48 @@ else
     record_type="AAAA"
 fi
 
-# Determines the current IP address
-new_ip=$($ip_curl http://ipecho.net/plain)
+if [[ ! -z $NEW_IP_ADDR ]]; then
+  # Specify IP Address from an environment variable
+  new_ip="$NEW_IP_ADDR"
+elif [[ ! -z $USE_ADAPTER ]]; then
+  # Check if extra tools is installed
+  command -v ifconfig >/dev/null 2>&1 || { echo >&2 "I require ifconfig but it's not installed.  Aborting."; exit 1; }
+  command -v awk >/dev/null 2>&1 || { echo >&2 "I require awk but it's not installed.  Aborting."; exit 1; }
+  command -v sed >/dev/null 2>&1 || { echo >&2 "I require sed but it's not installed.  Aborting."; exit 1; }
 
-# IP address service fallbacks
-if [[ -z $new_ip ]]; then
-    new_ip=$($ip_curl http://whatismyip.akamai.com)
-fi
-if [[ -z $new_ip ]]; then
-    new_ip=$($ip_curl http://icanhazip.com/)
-fi
-if [[ -z $new_ip ]]; then
-    new_ip=$($ip_curl https://tnx.nl/ip)
+  # Get the ip address from an adapter
+  new_ip=`echo `ifconfig "$USE_ADAPTER" 2>/dev/null|awk '/inet / {print $2}' | sed 's/inet //'``
+else
+  if [[ ! -z "$CUSTOM_IP_URL" ]]; then
+    new_ip=$($ip_curl "$CUSTOM_IP_URL")
+  else
+    # Determines the current IP address externally
+    new_ip=$($ip_curl http://ipecho.net/plain)
+
+    # IP address service fallbacks
+    if [[ -z $new_ip ]]; then
+      new_ip=$($ip_curl http://whatismyip.akamai.com)
+    fi
+    if [[ -z $new_ip ]]; then
+      new_ip=$($ip_curl http://icanhazip.com/)
+    fi
+    if [[ -z $new_ip ]]; then
+      new_ip=$($ip_curl https://tnx.nl/ip)
+    fi
+  fi
 fi
 
 if [[ -z $new_ip ]]; then
-    echo >&2 "Error: Unable to reach any service to determine the IP address."
-    exit 1
+  echo >&2 "Error: Unable to reach any service to determine the IP address."
+  exit 1
 fi
+
+echo $new_ip
 
 # Compares with last IP address set, if any
-ip_file="ip.dat"
-if [[ -f $ip_file ]]; then
-    ip=$(<$ip_file)
-    if [[ $ip = "$new_ip" ]]; then
+if [[ -f "$IP_FILE" ]]; then
+    ip=$(<"$IP_FILE")
+    if [[ $ip == "$new_ip" ]]; then
         echo "IP is unchanged : $ip. Exiting."
         exit 0
     fi
@@ -64,11 +115,18 @@ fi
 ip="$new_ip"
 echo "IP: $ip"
 
-# Fetches the zone information for the account
-zone_response=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$ZONE" \
+if [[ "$MASTER_API_KEY" == "true" ]]; then
+  # Fetches the zone information for the account
+  zone_response=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$ZONE" \
         -H "X-Auth-Email: $EMAIL" \
         -H "X-Auth-Key: $API" \
         -H "Content-Type: application/json")
+else
+  # Fetches the zone information for the account
+  zone_response=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$ZONE" \
+        -H "Authorization: Bearer $API" \
+        -H "Content-Type: application/json")
+fi
 
 
 # If not successful, errors out
@@ -81,7 +139,6 @@ fi
 # Selects the zone id
 zone_id=$(jq <<<"$zone_response" -r ".result[0].id")
 
-
 # If no zone id was found for the account, errors out.
 if [[ -z $zone_id ]]; then
     echo >&2 "Error: Could not determine Zone ID for $ZONE"
@@ -91,11 +148,18 @@ fi
 echo "Zone $ZONE id : $zone_id"
 
 
-# Tries to fetch the record of the host
-dns_record_response=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records?name=$HOST" \
+if [[ "$MASTER_API_KEY" == "true" ]]; then
+  # Tries to fetch the record of the host using Master API Key
+  dns_record_response=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records?name=$HOST" \
         -H "X-Auth-Email: $EMAIL" \
         -H "X-Auth-Key: $API" \
         -H "Content-Type: application/json")
+else
+  # Tries to fetch the record of the host using API Token
+  dns_record_response=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records?name=$HOST" \
+        -H "Authorization: Bearer $API" \
+        -H "Content-Type: application/json")
+fi
 
 if [[ $(jq <<<"$dns_record_response" -r '.success') != "true" ]]; then
     messages=$(jq <<<"$dns_record_response" -r '[.errors[] | .message] |join(" - ")')
@@ -136,26 +200,40 @@ if [[ -z $dns_record_id ]]; then
     # Creates a new record
     echo "Creating new record for host $HOST"
 
-    dns_record_response=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records" \
+    if [[ "$MASTER_API_KEY" == "true" ]]; then
+      dns_record_response=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records" \
             -H "X-Auth-Email: $EMAIL" \
             -H "X-Auth-Key: $API" \
             -H "Content-Type: application/json" \
             --data "$new_dns_record")
+    else
+      dns_record_response=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records" \
+            -H "Authorization: Bearer $API" \
+            -H "Content-Type: application/json" \
+            --data "$new_dns_record")
+    fi
 else
     # If a record is found, updates the existing record
     echo "Updating record $dns_record_id for host $HOST"
 
-    dns_record_response=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record_id" \
+    if [[ "$MASTER_API_KEY" == "true" ]]; then
+      dns_record_response=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record_id" \
             -H "X-Auth-Email: $EMAIL" \
             -H "X-Auth-Key: $API" \
             -H "Content-Type: application/json" \
             --data "$new_dns_record")
+    else
+      dns_record_response=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record_id" \
+            -H "Authorization: Bearer $API" \
+            -H "Content-Type: application/json" \
+            --data "$new_dns_record")
+    fi
 fi
 
 if [[  $(jq <<<"$dns_record_response" -r '.success') = "true" ]]; then
     # Records the IP set set if successful
     echo "IP changed to: $ip"
-    echo "$ip" > $ip_file
+    echo "$ip" > "$IP_FILE"
 else
     # Prints out error messages if unsuccessful
     messages=$(jq <<<"$dns_record_response" -r '[.errors[] | .error.message] |join(" - ")')
